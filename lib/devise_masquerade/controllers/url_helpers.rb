660193d2d1de7f61492b5cea751621d7b779521c
David Balatero <dbalatero@gmail.com>
2018-09-21 14:14:46 -0700

    Pass through additional parameters to path helpers

--- lib/devise_masquerade/controllers/url_helpers.rb
+++ lib/devise_masquerade/controllers/url_helpers.rb
@@ -1,14 +1,14 @@
 module DeviseMasquerade
   module Controllers
     module UrlHelpers
-      def masquerade_path(resource)
+      def masquerade_path(resource, *args)
         scope = Devise::Mapping.find_scope!(resource)
-        send("#{scope}_masquerade_path", resource)
+        send("#{scope}_masquerade_path", resource, *args)
       end
 
-      def back_masquerade_path(resource)
+      def back_masquerade_path(resource, *args)
         scope = Devise::Mapping.find_scope!(resource)
-        send("back_#{scope}_masquerade_index_path")
+        send("back_#{scope}_masquerade_index_path", *args)
       end
     end
   end
