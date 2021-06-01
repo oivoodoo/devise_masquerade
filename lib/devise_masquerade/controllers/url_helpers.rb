c920a0fc37d6bf1eb745aed82368bae6e0bf500a
Olle Jonsson <olle.jonsson@gmail.com>
2021-06-01 12:23:11 +0200

    refactor: avoid Hash#merge! for 1 argument
    Arguably, this is easier to read.

--- lib/devise_masquerade/controllers/url_helpers.rb
+++ lib/devise_masquerade/controllers/url_helpers.rb
@@ -8,9 +8,9 @@ module DeviseMasquerade
         scope = Devise::Mapping.find_scope!(resource)
 
         opts = args.shift || {}
-        opts.merge!(masqueraded_resource_class: resource.class.name)
+        opts[:masqueraded_resource_class] = resource.class.name
 
-        opts.merge!(Devise.masquerade_param => resource.masquerade_key)
+        opts[Devise.masquerade_param] = resource.masquerade_key
 
         send("#{scope}_masquerade_index_path", opts, *args)
       end
@@ -19,7 +19,7 @@ module DeviseMasquerade
         scope = Devise::Mapping.find_scope!(resource)
 
         opts = args.first || {}
-        opts.merge!(masqueraded_resource_class: resource.class.name)
+        opts[:masqueraded_resource_class] = resource.class.name
 
         send("back_#{scope}_masquerade_index_path", opts, *args)
       end
